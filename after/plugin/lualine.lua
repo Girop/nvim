ed59cc482296a90c2c604df6537fae9ea4bc171c
unknown <pigor2400@gmail.com>
2023-03-25 16:12:51 +0100

    new theme

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -4,6 +4,6 @@ if (not status) then return end
 lualine.setup {
     options = {
         icons_enabled = true,
-        theme = 'gruvbox'
+        theme = 'auto'
     }
 }
--- lua/ig/lazy.lua
+++ lua/ig/lazy.lua
@@ -12,13 +12,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    -- themes
-    'fcpg/vim-fahrenheit',
-    'artanikin/vim-synthwave84',
     'sainnhe/gruvbox-material',
-    'Mofiqul/dracula.nvim',
-    'folke/tokyonight.nvim',
-    -- 
+    { "catppuccin/nvim", name = "catppuccin" },
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
     'nvim-telescope/telescope.nvim',
@@ -67,8 +62,59 @@ local opts = {
     }
 }
 require('lazy').setup(plugins,opts)
-vim.o.background = 'dark'
-vim.cmd[[let g:gruvbox_material_better_performance = 1]]
-vim.cmd[[let g:gruvbox_material_foreground = 'original']]
-vim.cmd[[let g:gruvbox_material_background = 'soft']]
-vim.cmd[[colorscheme gruvbox-material]]
+
+require("catppuccin").setup({
+    flavour = "mocha",
+    background = {
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = true,
+    show_end_of_buffer = false,
+    term_colors = false,
+    dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+    },
+    styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {
+        mocha = {
+            text = "#CC99FF",
+            subtext1 = "#DEBAD4",
+            subtext0 = "#C8A6BE",
+            overlay2 = "#B293A8",
+            overlay1 = "#9C7F92",
+            overlay0 = "#866C7D",
+            surface2 = "#705867",
+            surface1 = "#5A4551",
+            surface0 = "#44313B",
+
+            base = "#352939",
+            mantle = "#211924",
+            crust = "#1a1016",
+        },
+    },
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+    },
+})
+
+vim.cmd.colorscheme "catppuccin"
