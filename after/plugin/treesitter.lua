8c321ba0a63b3bd9080aa95e0113c25009df4f2b
unknown <pigor2400@gmail.com>
2023-03-29 19:59:05 +0200

    treesitter working

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,1 +1,16 @@
-require('nvim-treesitter.configs').setup{}
+require('nvim-treesitter.configs').setup{
+    ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "help",
+        "query",
+        "python",
+        "go"
+    },
+    auto_install = true,
+    highlight = {
+        enable = true
+    },
+    additional_vim_regex_highlighting = false,
+}
