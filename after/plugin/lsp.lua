beb7dec9913b333db81ec565aebcd11bd72aea0d
unknown <pigor2400@gmail.com>
2023-04-05 11:05:15 +0200

    Formating shortcut

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<M-k>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<M-j>'] = cmp.mapping.select_next_item(cmp_select),
@@ -10,10 +10,10 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ["<C-s>"] = cmp.mapping.complete(),
 })
 
-lsp.setup_nvim_cmp{mapping = cmp_mappings}
+lsp.setup_nvim_cmp { mapping = cmp_mappings }
 
 lsp.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
     vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -30,28 +30,32 @@ lsp.setup()
 
 local null_ls = require('null-ls')
 local null_opts = lsp.build_options('null-ls', {})
-local null_builtins = 
-null_ls.setup({
-    on_attach = function(client, bufnr)
-        null_opts.on_attach(client, bufnr)
+local null_builtins =
+    null_ls.setup({
+        on_attach = function(client, bufnr)
+            null_opts.on_attach(client, bufnr)
 
-        local format_cmd = function(input)
-            vim.lsp.buf.format({
-                id = client.id,
-                timeout_ms = 5000,
-                async = input.bang,
+            local format_cmd = function(input)
+                vim.lsp.buf.format({
+                    id = client.id,
+                    timeout_ms = 5000,
+                    async = input.bang,
+                })
+            end
+
+            local bufcmd = vim.api.nvim_buf_create_user_command
+            bufcmd(bufnr, 'NullFormat', format_cmd, {
+                bang = true,
+                range = true,
+                desc = 'Format using null-ls'
             })
-        end
+        end,
+        sources = {
+            null_ls.builtins.formatting.isort,
+            null_ls.builtins.formatting.black,
+        }
+    })
 
-        local bufcmd = vim.api.nvim_buf_create_user_command
-        bufcmd(bufnr, 'NullFormat', format_cmd, {
-            bang = true,
-            range = true,
-            desc = 'Format using null-ls' })
-    end,
-    sources = {
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.black,
-
-    }
-})
+vim.keymap.set("n", "<leader>k", function()
+    vim.lsp.buf.format()
+end)
