7394d8bb00ab6a0f0142c0a8a1a190fbbcb253c3
unknown <pigor2400@gmail.com>
2023-04-09 20:09:47 +0200

    Alternate file

--- lua/ig/remap.lua
+++ lua/ig/remap.lua
@@ -22,7 +22,7 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set("n","<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>p", "\"_dP")
 
 vim.keymap.set("i", "<C-Del>", "<C-o>dw")
 
@@ -40,5 +40,6 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 
-vim.keymap.set('n','<leader><leader>','<c-o>')
-
+vim.keymap.set('n', '<leader><leader>', function()
+    vim.cmd("e #")
+end)
